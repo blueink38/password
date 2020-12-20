f8b107c736987419d4530256c59a80489b69b35e
muncle <blueink38@yahoo.com>
2020-12-20 00:07:22 -0600

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# password
+# Password
+> This project allows a user to create a unique password of a desired length using and combination of numbers, letters, and symbols.
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+In a world with the increased need of online security, I have created a simple, streamlined webite that will generate a unique password for the user.  The user can choose the length from 8-128 characters long and with any combination of upper case or lower case letters as well as numbers and symbols.
+
+## Screenshots
+![Example screenshot](./img/screenshot.png)
+
+## Technologies
+* HTML5
+* CSS3
+* JavaScript 1.8.5
+
+## Setup
+This website can be found using the link at the bottom of the README file.
+
+## Code Examples
+Show examples of usage:
+`return String.fromCharCode(Math.floor(Math.random() * 26) + 97);`
+
+## Features
+List of features ready and TODOs for future development
+* Allows customization of the length of the password
+* Uses any combination of text, numbers, and symbols
+* Produces a unique password anytime it is used
+
+To-do list:
+* Add a clipboard function
+* Save password information
+
+## Status
+Project is complete but new updates may come as need and technology grow
+
+## Inspiration
+This was inspired by a desire to improve JavaScript skills.
+
+## Contact
+Created by [blueink38@yahoo.com](https://www.github.com/blueink38) - feel free to contact me!
--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 // Assignment code here
 
+// Elements
+const yourPasswordElement = document.getElementById('output');
+const yourLengthElement = document.getElementById('lenth');
+const yourUpperElement = document.getElementById('upper');
+const yourLowerElement = document.getElementById('lower');
+const yourNumberElement = document.getElementById('number');
+const yourSymbolElement = document.getElementById('symbol');
+
+
+
+// randomizing function
+const randomizer = {
+  getuppercase: randomUpper,
+  getlowercase: randomLower,
+  getnumber: randomNumber,
+  getsymbol: randomSymbol,
+};
+
+generateElement.addEventListener('click', () => {
+  const length = +yourLengthElement.value;
+  const hasUpperCase = yourUpperElement.checked;
+  const hasLowerCase = yourLowerElement.checked;
+  const hasNumber = yourNumberElement.checked;
+  const hasSymbol = yourSymbolElement.checked;
+
+  console.log(hasUpperCase, hasLowerCase, hasNumber, hasSymbol);
+});
+
+
+
 // Choose LENGTH of password 8-128
-function passwordLength() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-console.log(passwordLength())
+// function passwordLength() {
+//  return number(passwordLengthNumber);
+//}
+ //console.log(passwordLength())
 
 // generate random LOWERCASE letter
 function randomLower() {
@@ -36,6 +66,17 @@ console.log(randomSymbol())
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener('click', () => {
+  const length = +yourLengthElement.value;
+  const upper = yourUpperElement.checked;
+  const lower = yourLowerElement.checked;
+  const number = yourNumberElement.checked;
+  const symbol = yourSymbolElement.checked;
+
+  console.log(length, upper, lower, number, symbol);
+});
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -46,4 +87,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", writePassword);
